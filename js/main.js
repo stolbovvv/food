a09f24654b483ce32bf3a0c935c524ad7381bd20
Valerii Stolbov <stolbov.v@outlook.com>
2022-08-21 02:44:55 +0500

    Updates

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 "use strict";
 
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && iter[Symbol.iterator] != null || iter["@@iterator"] != null) return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
+
 window.addEventListener('DOMContentLoaded', function () {
   // script: tabs
   var tabsItems = document.querySelectorAll('.tabheader__item');
@@ -165,5 +183,89 @@ window.addEventListener('DOMContentLoaded', function () {
     }
   }
 
-  window.addEventListener('scroll', showModalByScroll);
+  window.addEventListener('scroll', showModalByScroll); // script: calsses
+
+  var MenuCard = /*#__PURE__*/function () {
+    function MenuCard(_ref) {
+      var src = _ref.src,
+          alt = _ref.alt,
+          title = _ref.title,
+          descr = _ref.descr,
+          price = _ref.price,
+          parentSelector = _ref.parentSelector,
+          _ref$classes = _ref.classes,
+          classes = _ref$classes === void 0 ? [] : _ref$classes;
+
+      _classCallCheck(this, MenuCard);
+
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.descr = descr;
+      this.price = price;
+      this.classes = classes;
+      this.parent = document.querySelector(parentSelector);
+      this.transfer = 65;
+      this.changeToRUB();
+    }
+
+    _createClass(MenuCard, [{
+      key: "changeToRUB",
+      value: function changeToRUB() {
+        this.price *= this.transfer;
+      }
+    }, {
+      key: "render",
+      value: function render() {
+        var _card$classList;
+
+        var parent = this.parent;
+        var classNames = this.classes;
+        var card = document.createElement('div');
+        var price = document.createElement('div');
+        if (!classNames.includes('menu__item')) classNames.push('menu__item');
+
+        (_card$classList = card.classList).add.apply(_card$classList, _toConsumableArray(classNames));
+
+        card.insertAdjacentHTML('beforeend', "<img src=".concat(this.src, " alt=").concat(this.alt, " />"));
+        card.insertAdjacentHTML('beforeend', "<h3 class=\"menu__item-subtitle\">".concat(this.title, "</h3>"));
+        card.insertAdjacentHTML('beforeend', "<p class=\"menu__item-descr\">".concat(this.descr, "</p>"));
+        card.insertAdjacentHTML('beforeend', "<div class=\"menu__item-divider\"></div>");
+        price.classList.add('menu__item-price');
+        price.insertAdjacentHTML('beforeend', "<div class=\"menu__item-cost\">\u0426\u0435\u043D\u0430:</div>");
+        price.insertAdjacentHTML('beforeend', "<div class=\"menu__item-total\"><span>".concat(this.price, "</span> \u0440\u0443\u0431/\u0434\u0435\u043D\u044C</div>"));
+        card.append(price);
+        parent.append(card);
+      }
+    }]);
+
+    return MenuCard;
+  }();
+
+  new MenuCard({
+    src: './images/tabs/vegy.jpg',
+    alt: 'vegy',
+    title: 'Меню "Фитнес"',
+    price: 9,
+    parentSelector: '.menu .container',
+    classes: ['menu__item', 'menu__item_new'],
+    descr: "\u041C\u0435\u043D\u044E \"\u0424\u0438\u0442\u043D\u0435\u0441\" - \u044D\u0442\u043E \u043D\u043E\u0432\u044B\u0439 \u043F\u043E\u0434\u0445\u043E\u0434 \u043A \u043F\u0440\u0438\u0433\u043E\u0442\u043E\u0432\u043B\u0435\u043D\u0438\u044E \u0431\u043B\u044E\u0434: \u0431\u043E\u043B\u044C\u0448\u0435 \u0441\u0432\u0435\u0436\u0438\u0445 \u043E\u0432\u043E\u0449\u0435\u0439 \u0438 \u0444\u0440\u0443\u043A\u0442\u043E\u0432. \u041F\u0440\u043E\u0434\u0443\u043A\u0442 \u0430\u043A\u0442\u0438\u0432\u043D\u044B\u0445 \u0438 \u0437\u0434\u043E\u0440\u043E\u0432\u044B\u0445 \u043B\u044E\u0434\u0435\u0439. \u042D\u0442\u043E \u0430\u0431\u0441\u043E\u043B\u044E\u0442\u043D\u043E \u043D\u043E\u0432\u044B\u0439 \u043F\u0440\u043E\u0434\u0443\u043A\u0442 \u0441 \u043E\u043F\u0442\u0438\u043C\u0430\u043B\u044C\u043D\u043E\u0439 \u0446\u0435\u043D\u043E\u0439 \u0438 \u0432\u044B\u0441\u043E\u043A\u0438\u043C \u043A\u0430\u0447\u0435\u0441\u0442\u0432\u043E\u043C!"
+  }).render();
+  new MenuCard({
+    src: './images/tabs/elite.jpg',
+    alt: 'elite',
+    title: 'Меню “Премиум”',
+    price: 21,
+    parentSelector: '.menu .container',
+    descr: "\u0412 \u043C\u0435\u043D\u044E \u201C\u041F\u0440\u0435\u043C\u0438\u0443\u043C\u201D \u043C\u044B \u0438\u0441\u043F\u043E\u043B\u044C\u0437\u0443\u0435\u043C \u043D\u0435 \u0442\u043E\u043B\u044C\u043A\u043E \u043A\u0440\u0430\u0441\u0438\u0432\u044B\u0439 \u0434\u0438\u0437\u0430\u0439\u043D \u0443\u043F\u0430\u043A\u043E\u0432\u043A\u0438, \u043D\u043E \u0438 \u043A\u0430\u0447\u0435\u0441\u0442\u0432\u0435\u043D\u043D\u043E\u0435 \u0438\u0441\u043F\u043E\u043B\u043D\u0435\u043D\u0438\u0435 \u0431\u043B\u044E\u0434. \u041A\u0440\u0430\u0441\u043D\u0430\u044F \u0440\u044B\u0431\u0430, \u043C\u043E\u0440\u0435\u043F\u0440\u043E\u0434\u0443\u043A\u0442\u044B, \u0444\u0440\u0443\u043A\u0442\u044B - \u0440\u0435\u0441\u0442\u043E\u0440\u0430\u043D\u043D\u043E\u0435 \u043C\u0435\u043D\u044E \u0431\u0435\u0437 \u043F\u043E\u0445\u043E\u0434\u0430 \u0432 \u0440\u0435\u0441\u0442\u043E\u0440\u0430\u043D!"
+  }).render();
+  new MenuCard({
+    src: './images/tabs/post.jpg',
+    alt: 'post',
+    title: 'Меню "Постное"',
+    price: 14,
+    parentSelector: '.menu .container',
+    classes: ['menu__item'],
+    descr: "\u041C\u0435\u043D\u044E \u201C\u041F\u043E\u0441\u0442\u043D\u043E\u0435\u201D - \u044D\u0442\u043E \u0442\u0449\u0430\u0442\u0435\u043B\u044C\u043D\u044B\u0439 \u043F\u043E\u0434\u0431\u043E\u0440 \u0438\u043D\u0433\u0440\u0435\u0434\u0438\u0435\u043D\u0442\u043E\u0432: \u043F\u043E\u043B\u043D\u043E\u0435 \u043E\u0442\u0441\u0443\u0442\u0441\u0442\u0432\u0438\u0435 \u043F\u0440\u043E\u0434\u0443\u043A\u0442\u043E\u0432 \u0436\u0438\u0432\u043E\u0442\u043D\u043E\u0433\u043E \u043F\u0440\u043E\u0438\u0441\u0445\u043E\u0436\u0434\u0435\u043D\u0438\u044F, \u043C\u043E\u043B\u043E\u043A\u043E \u0438\u0437 \u043C\u0438\u043D\u0434\u0430\u043B\u044F, \u043E\u0432\u0441\u0430, \u043A\u043E\u043A\u043E\u0441\u0430 \u0438\u043B\u0438 \u0433\u0440\u0435\u0447\u043A\u0438, \u043F\u0440\u0430\u0432\u0438\u043B\u044C\u043D\u043E\u0435 \u043A\u043E\u043B\u0438\u0447\u0435\u0441\u0442\u0432\u043E \u0431\u0435\u043B\u043A\u043E\u0432 \u0437\u0430 \u0441\u0447\u0435\u0442 \u0442\u043E\u0444\u0443 \u0438 \u0438\u043C\u043F\u043E\u0440\u0442\u043D\u044B\u0445 \u0432\u0435\u0433\u0435\u0442\u0430\u0440\u0438\u0430\u043D\u0441\u043A\u0438\u0445 \u0441\u0442\u0435\u0439\u043A\u043E\u0432."
+  }).render();
 });
